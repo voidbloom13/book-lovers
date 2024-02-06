2d28f0930fa15550df17119689f7b4308b0a5367
voidbloom <dakota.huyghe.dev@gmail.com>
2024-02-06 04:41:35 -0500

    Moved navigation code to NavigationTabs.js, added Login.js, and added temporary conditional logic to display either the Login screen or the Navigation screen

--- App.js
+++ App.js
@@ -1,113 +1,13 @@
-import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-// Navigation Imports
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from '@expo/vector-icons/Ionicons';
-
-// Added temporary components to display navigation tabs, will be replaced as progress is made.
-
-const Page1 = () => {
-  return (
-    <View style={styles.pageView}>
-      <Text style={styles.pageText}>Page1</Text>
-    </View>
-  )
-}
-
-const Page2 = () => {
-  return (
-    <View style={styles.pageView}>
-      <Text style={styles.pageText}>Page2</Text>
-    </View>
-  )
-}
-
-const NewItem = () => {
-  return (
-    <View style={styles.pageView}>
-      <Text style={styles.pageText}>New Item</Text>
-    </View>
-  )
-}
-
-const Page3 = () => {
-  return (
-    <View style={styles.pageView}>
-      <Text style={styles.pageText}>Page3</Text>
-    </View>
-  )
-}
-
-const Page4 = () => {
-  return (
-    <View style={styles.pageView}>
-      <Text style={styles.pageText}>Page4</Text>
-    </View>
-  )
-}
-
-const Tab = createBottomTabNavigator();
+import React, {useState} from 'react';
+import Login from './src/Login'
+import NavigationTabs from './src/NavigationTabs';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size}) => {
-            let iconName;
-            let iconSize = 24;
-            let iconColor;
-
-            if (route.name === 'Home') {
-              iconName = focused ? 'home-sharp' : 'home-outline';
-              iconColor = '#5050ff';
-            } else if (route.name === 'My Books') {
-              iconName = focused ? 'star' : 'star-outline';
-              iconColor = '#f0f700';
-            } else if (route.name === 'New Item') {
-              iconName = focused ? 'add-circle-sharp' : 'add-circle-outline';
-              iconSize = 40;
-              iconColor = '#e95429';
-            } else if (route.name === 'My Readlist') {
-              iconName = focused ? 'book-sharp' : 'book-outline';
-              iconColor = '#00a700';
-            } else if (route.name === 'Browse') {
-              iconName = focused ? 'search-sharp' : 'search-outline';
-              iconColor = '#ff0000';
-            }
-
-            return <Ionicons name={iconName} size={iconSize} color={iconColor} />
-          },
-          tabBarActiveBackgroundColor: '#292930',
-          tabBarInactiveBackgroundColor: '#1f1f25',
-          tabBarActiveTintColor: '#c5c5c5',
-          tabBarLabelStyle: {
-            fontSize: 12
-          }
-        })}
-      >
-        <Tab.Screen name='Home' component={Page1} />
-        <Tab.Screen name='My Books' component={Page2} />
-        <Tab.Screen name='New Item' component={NewItem} />
-        <Tab.Screen name='My Readlist' component={Page3} />
-        <Tab.Screen name='Browse' component={Page4} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    isLoggedIn ? <NavigationTabs /> : <Login onPress={() => {setIsLoggedIn(true)}} />
   )
 }
-
-
-const styles = StyleSheet.create({
-  pageView: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  pageText: {
-    alignSelf: 'center',
-    fontSize: 30
-  }
-})
 
 export default App;
---
+++ src/NavigationTabs.js
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// Navigation Imports
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+// Added temporary components to display navigation tabs, will be replaced as progress is made.
+
+// TODO: Add conditional logic to App.js to determine if user is logged in and render either Login component or 
+// LoggedIn screen with Tab Navigation. Move Tab Navigation to LoggedIn Screen. Make New Item tab open a modal to
+// add a new book to your collection or read list.
+
+const Page1 = () => {
+  return (
+    <View style={styles.pageView}>
+      <Text style={styles.pageText}>Page1</Text>
+    </View>
+  )
+}
+
+const Page2 = () => {
+  return (
+    <View style={styles.pageView}>
+      <Text style={styles.pageText}>Page2</Text>
+    </View>
+  )
+}
+
+const NewItem = () => {
+  return (
+    <View style={styles.pageView}>
+      <Text style={styles.pageText}>New Item</Text>
+    </View>
+  )
+}
+
+const Page3 = () => {
+  return (
+    <View style={styles.pageView}>
+      <Text style={styles.pageText}>Page3</Text>
+    </View>
+  )
+}
+
+const Page4 = () => {
+  return (
+    <View style={styles.pageView}>
+      <Text style={styles.pageText}>Page4</Text>
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator();
+
+const NavigationTabs = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size}) => {
+            let iconName;
+            let iconSize = 24;
+            let iconColor;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home-sharp' : 'home-outline';
+              iconColor = '#5050ff';
+            } else if (route.name === 'My Books') {
+              iconName = focused ? 'star' : 'star-outline';
+              iconColor = '#f0f700';
+            } else if (route.name === 'New Item') {
+              iconName = focused ? 'add-circle-sharp' : 'add-circle-outline';
+              iconSize = 40;
+              iconColor = '#e95429';
+            } else if (route.name === 'My Readlist') {
+              iconName = focused ? 'book-sharp' : 'book-outline';
+              iconColor = '#00a700';
+            } else if (route.name === 'Browse') {
+              iconName = focused ? 'search-sharp' : 'search-outline';
+              iconColor = '#ff0000';
+            }
+
+            return <Ionicons name={iconName} size={iconSize} color={iconColor} />
+          },
+          tabBarActiveBackgroundColor: '#000000',
+          tabBarInactiveBackgroundColor: '#000000',
+          tabBarActiveTintColor: '#c5c5c5',
+          tabBarLabelStyle: {
+            fontSize: 12
+          }
+        })}
+      >
+        <Tab.Screen name='Home' component={Page1} />
+        <Tab.Screen name='My Books' component={Page2} />
+        <Tab.Screen name='New Item' component={NewItem} />
+        <Tab.Screen name='My Readlist' component={Page3} />
+        <Tab.Screen name='Browse' component={Page4} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  pageView: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  pageText: {
+    alignSelf: 'center',
+    fontSize: 30
+  }
+})
+
+export default NavigationTabs;
