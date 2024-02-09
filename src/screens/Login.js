e97639867a96fa4d2617cfa78d3659f7c8ce423a
voidbloom <dakota.huyghe.dev@gmail.com>
2024-02-09 04:29:20 -0500

    Added Theme styling to Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,36 +1,19 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useTheme } from '../ThemeContext';
+import { StylesLight, StylesDark } from '../../public/Styles';
 
 const Login = ({onPress}) => {
+  const darkMode = useTheme();
+  const Styles = darkMode ? StylesDark : StylesLight;
+
   return (
-    <View style={styles.screenContainer}>
-      <TouchableOpacity style={styles.loginButton} onPress={() => {onPress()}}>
-        <Text style={styles.loginText}>Login to view the App!</Text>
+    <View style={Styles.screenContainer}>
+      <TouchableOpacity style={Styles.primaryButton} onPress={() => {onPress()}}>
+        <Text style={Styles.primaryButtonText}>Login to view the App!</Text>
       </TouchableOpacity>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    alignContent: 'center',
-    justifyContent: 'center'
-  },
-  loginText: {
-    color: 'white',
-    fontSize: 30,
-    textAlign: 'center'
-  },
-  loginButton: {
-    alignSelf: 'center',
-    backgroundColor: 'blue',
-    borderWidth: 1,
-    borderColor: 'blue',
-    borderRadius: 8,
-    width: '75%',
-    padding: 8
-  }
-})
 
 export default Login;
